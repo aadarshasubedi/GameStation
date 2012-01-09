41e795ad5b0fae8ad8c81e251a3c1894ded19b34
OspreyWarrior <mrblankface20002@yahoo.com>
2012-01-09 02:15:57 -0700

    first commit

---
+++ GameStation/Game Station/Game Station/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.browser = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(41, 278)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(256, 117)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select A Student Game"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(442, 552)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(251, 45)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Send Feedback"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(363, 100)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(5)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(446, 442)
+        Me.TextBox1.TabIndex = 2
+        '
+        'browser
+        '
+        Me.browser.DefaultExt = "exe"
+        Me.browser.FileName = "Select a game"
+        Me.browser.Filter = "Executable files|*.exe"
+        Me.browser.InitialDirectory = "C:\Program Files\OGRE\Game Station\Games"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(477, 70)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(200, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Submit your feedback!"
+        '
+        'Timer1
+        '
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(328, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 25)
+        Me.Label2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(723, -1)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 48)
+        Me.Label3.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(849, 745)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Game Station"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents browser As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ GameStation/Game Station/Game Station/Form1.vb
@@ -0,0 +1,180 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ''define global variables
+
+    '' This is for the timer
+    Dim limit As Integer = 0
+    Dim timerOn As Boolean = True
+    ''This is the location of the .ini file.
+    Dim inifile As String = Application.StartupPath + "\Game Station.ini"
+    ''This is the location of the text document, to be determined from the .ini file
+    Dim ffile As String
+
+    Dim file As String
+    Dim game As Process
+
+
+
+
+    ''This reads what's in the feedback file.
+    Public Function GetFileContents(ByVal FullPath As String, Optional ByRef ErrInfo As String = "Incorrect path") As String
+        Dim strContents As String
+        Dim objReader As StreamReader
+        Try
+            
+            objReader = New StreamReader(FullPath)
+            strContents = objReader.ReadToEnd()
+            objReader.Close()
+            Return strContents
+        Catch ex As Exception
+            ErrInfo = ex.Message
+        End Try
+    End Function
+ 
+    'loads the form  reads from .ini file
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+        Dim ini As String
+        Dim objReader As StreamReader
+
+        TextBox1.Text = "Enter your feedback here."
+
+        'read from the .ini file
+        objReader = New StreamReader(inifile)
+        ini = objReader.ReadToEnd()
+        objReader.Close()
+
+
+        'create the directory
+        If My.Computer.FileSystem.DirectoryExists(ini) = False Then
+            My.Computer.FileSystem.CreateDirectory(ini)
+        End If
+
+        'adds onto the path
+        ini = (ini + "feedback.txt")
+
+        'saves the path to ffile so it can be used elsewhere
+        ffile = ini
+
+        ' create the file
+        If My.Computer.FileSystem.FileExists(ffile) = False Then
+            My.Computer.FileSystem.WriteAllText(ffile, "", False)
+        End If
+
+    End Sub
+    'The button to browse for a game
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        browser.InitialDirectory = "C:\Program Files\OGRE\Game Station\Games"
+        browser.ShowDialog()
+        TextBox1.Text = "Enter your feedback here."
+
+    End Sub
+    ' This sub will start the timer, send "ffile" to the public function so it can read the feedback file and start the game
+    Private Sub browser_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles browser.FileOk
+        Dim file As String
+        'file is the string of what you browse for
+        file = browser.FileName
+
+        Call GetFileContents(ffile)
+
+        Call Timer_Start()
+
+        game = Process.GetProcessById(Shell(file))
+
+    End Sub
+    'This sub saves feedback to the textfile "feedback.txt" and saves it with a date stamp, gives an error message if nessecary. 
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+
+        Dim sContents As String
+        Dim slashIndex As Integer
+
+        Dim objReader As StreamWriter
+
+        If TextBox1.Text.Contains("Feedback sent!") Or TextBox1.Text.Contains("Enter your feedback here.") = False Then
+            Try
+
+                'formatting
+                slashIndex = (browser.FileName.LastIndexOf("\") + 1)
+                file = browser.FileName.Remove(0, slashIndex)
+                sContents = GetFileContents(ffile) + DateAndTime.DateString + " " + TimeOfDay + " " + file + " -- " + TextBox1.Text.Trim()
+
+                ''writes over what's inside the feedback file.
+                objReader = New StreamWriter(ffile)
+                objReader.WriteLine(sContents)
+                objReader.Close()
+                TextBox1.Text = "Feedback sent!"
+            Catch
+                TextBox1.Text = "You must play a game first in order to send feedback!"
+
+            End Try
+        End If
+
+
+    End Sub
+    'erase the text if you click on the text box to enter text
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.Click
+        TextBox1.Text = ""
+
+    End Sub
+    '' a "secret" way to close the program, because we don't want people closing it when they're done.
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        Me.Close()
+    End Sub
+    ''every minute the timer ticks, then when it reaches five minutes, it calls to kill the process.
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+
+        limit = limit + 1
+        If limit = 5 Then
+
+            Call Kill_Process()
+
+        End If
+
+
+
+    End Sub
+    ''starts the timer and makes each tick last a minute
+    Private Sub Timer_Start()
+        If Timer1.Enabled = True Then
+            Timer1.Interval = 60 * 1000
+        End If
+
+
+    End Sub
+    ''kills the process with some error checking in case you close the process before the timer is enabled.
+    Private Sub Kill_Process()
+        limit = 0
+        Dim oops As String = "oops"
+        Try
+            game.Kill()
+            Timer1.Enabled = False
+
+
+        Catch ex As Exception
+            oops = ex.Message
+
+        End Try
+
+
+
+
+
+
+    End Sub
+    'a secret way to disable the timer. The label is in the top right corner
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+
+
+        If timerOn = True Then
+            timerOn = False
+            TextBox1.Text = "Timer disabled"
+        Else
+            timerOn = True
+            TextBox1.Text = "Timer enabled"
+        End If
+    End Sub
+End Class
